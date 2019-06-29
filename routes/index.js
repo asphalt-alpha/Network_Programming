72eaaae1b9f6023d7186bb0d68f4148f0c70c8c2
asphalt-alpha <ddolbangpark@gmail.com>
2019-06-30 02:53:54 +0900

    [add] login views
    부트스트랩 적용, 굉장히 간단한 로그인 화면만 구성

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'See, the Sea' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'See, the Sea' });
 });
 
 module.exports = router;
