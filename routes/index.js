6924432e8107c7f9e21e8da10fb3084d27bd8f04
asphalt-alpha <ddolbangpark@gmail.com>
2019-07-10 23:01:49 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var camConfig = require('../model/camConfig');
 var fs = require('fs');
 
 function setLED(flag) {
-  fs.open('/dev/ttyUSB1','a', 666, function(e, fd){  //쓰기로 열기
+  fs.open('/dev/ttyUSB1','a', 666, function(err, fd){  //쓰기로 열기
     if(err){
       res.redirect('/cam');
     }
